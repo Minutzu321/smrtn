fd541d6dc129a01498069b6bc39b224294890eb4
Chirus Mina <chirusmina@gmail.com>
2021-07-30 00:10:36 +0300

    Update

--- backend/models.py
+++ backend/models.py
@@ -5,14 +5,14 @@ from django.utils import timezone
 
 # Create your models here.
 
+class IP(models.Model):
+    ip = models.GenericIPAddressField()
+    ora_data = models.DateTimeField(default=timezone.now)
+
 class Utilizator(models.Model):
     nume = models.CharField(max_length=100)
     email = models.EmailField()
     cod_secret = models.UUIDField(default=uuid.uuid4())
     prima_logare = models.DateTimeField(default=timezone.now)
     ultima_logare = models.DateTimeField(default=timezone.now)
-    ips = models.ManyToManyRel
-
-class IP(models.Model):
-    ip = models.GenericIPAddressField()
-    ora_data = models.DateTimeField(default=timezone.now)
+    ipuri = models.ManyToManyField(IP, blank=True)
--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
+#!/bin/bash
 python manage.py makemigrations
 python manage.py migrate
-python manage.py sqlmigrate backend 0001
-sudo supervisorctl restart all
+python manage.py sqlmigrate backend 0002
